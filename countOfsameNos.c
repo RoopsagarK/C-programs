4ab27b12ae53530033ba5e4f9622d0283433554d
Roopsagar K <107497296+RoopsagarK@users.noreply.github.com>
2022-07-24 13:56:31 -0700

    Update countOfsameNos.c

--- countOfsameNos.c
+++ countOfsameNos.c
@@ -1,3 +1,5 @@
+//To find out total number of times the number 'x' occurs in a list of array
+
 #include<stdio.h>
 int main(){
 	int m;
@@ -21,3 +23,9 @@ int main(){
     
 	return 0;
 }
+
+/*output :
+enter the size of the array : 5
+enter the array elements : 1 2 2 3 2
+enter the number : 2
+Count = 3 */
