39a8caa2a304c6e2d3b4432810aeec459a6f955a
Roopsagar K <107497296+RoopsagarK@users.noreply.github.com>
2022-07-24 02:34:09 -0700

    Add files via upload
    Array

---
+++ array2Dio.c
@@ -0,0 +1,26 @@
+//taking input and printing output of 2d array//
+
+#include<stdio.h>
+void main(){
+	int r,c;
+	printf("enter the size of row and column : ");
+	scanf("%d%d",&r,&c);
+	int a[r][c];
+	printf("enter the array elements : ");
+	for(int i=0;i<r;i++){;
+		for(int j=0;j<c;j++){
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(int i=0;i<r;i++){
+		for(int j=0;j<c;j++){
+			printf("%d ",a[i][j]);
+		}
+		printf("\n");
+	}	
+}
+
+/*enter the size of row and column : 2 2
+enter the array elements : 1 2 3 4
+1 2
+3 4   */
